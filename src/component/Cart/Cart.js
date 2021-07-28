7b93ad0f1f15f0b4b0036241afdc8519506770e5
Robin <mdrobinhossain12@gmail.com>
2021-07-29 02:05:01 +0600

    last module34 commit

---
+++ src/component/Cart/Cart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    const total = cart.reduce((total,prd)=> total + prd.price,0);
+    return (
+        <div>
+            <h4>Order summary</h4>
+            <p>Items Ordered: {cart.length}</p>
+            <p>Total price: ${total}</p>
+        </div>
+    );
+};
+
+export default Cart;
